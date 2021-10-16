c2b273285cce3a1e79ea462482df3f4bf0841957
Wesley Silva <88410575+jwesleylima@users.noreply.github.com>
2021-10-16 17:04:57 -0300

    Arquivo principal adicionado

---
+++ bin2dec.py
@@ -0,0 +1,33 @@
+"""Module written on 08/22/2021 by JWesleyLima.
+Visit my profile: https://github.com/jwesleylima."""
+
+
+def bin2dec(*, binary_digits: str) -> int:
+	"""Converts binary digits into decimal numbers.
+
+	- Keyword Parameters:
+		binary_digits: Binary digits as a string.
+
+	- Return:
+		int: Decimal number
+	
+	- Usage Example:
+		some_decimal = bin2dec(binary_digits='0101')
+	"""
+	binary_digits = binary_digits.strip().replace(' ', '')
+	VALID_BINARY_DIGITS = ('0', '1')
+
+	if not isinstance(binary_digits, str):
+		raise TypeError('Binary digits must be of type \'str\'')
+	elif len(binary_digits) == 0:
+		raise ValueError('An empty string was entered')
+	elif not str(binary_digits).isnumeric():
+		raise ValueError(f'Unexpected invalid binary digit: "{binary_digits}"')
+	elif not all(d in VALID_BINARY_DIGITS for d in binary_digits):
+		raise ValueError(f'Unknown binary digit format: "{binary_digits}"')
+
+	# Convert
+	decimal_number = 0
+	for i, digit in enumerate(binary_digits[::-1]):
+		decimal_number += (int(digit) * 2 ** i)
+	return decimal_number
